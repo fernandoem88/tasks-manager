7268aaa8e885d0fd577623e3069d68dfc4a15b12
Fernando EM <vs_fernando@yahoo.fr>
2024-10-14 09:18:29 +0200

    handle new task in dispatch utility

--- src/contexts/AppStateProvider/hooks/useDispatch.ts
+++ src/contexts/AppStateProvider/hooks/useDispatch.ts
@@ -1,11 +1,14 @@
 import { getUniqId } from "@/utils/getUniqId";
 import { useAppState, useSetAppState } from "..";
-import type { Board, BoardColumn } from "@/types";
+import type { Board, BoardColumn, Task } from "@/types";
 import { produce } from "immer";
+import { DateTime } from "luxon";
 
 interface ActionPayload {
   newBoard: string;
   newBoardColumn: { columnName: string; boardId: string };
+  editBoardColumnName: { id: string; name: string };
+  newTask: { taskName: string; columnId: string; description?: string };
 }
 
 export const useDispatch = () => {
@@ -28,15 +31,52 @@ export const useDispatch = () => {
     columnName,
   }: ActionPayload["newBoardColumn"]) => {
     const id = getUniqId("column-");
-    const board: BoardColumn = {
+    const column: BoardColumn = {
       name: columnName,
       id,
       boardId,
       tasksIds: [],
     };
     const newState = produce(appState, (draft) => {
-      draft.columns[id] = board;
-      draft.boards[boardId].columnIds.push(id);
+      draft.columns[id] = column;
+      draft.boards[boardId]?.columnIds.push(id);
+    });
+    setState(newState);
+    return newState;
+  };
+
+  const handleEditBoardColumnName = ({
+    name,
+    id,
+  }: ActionPayload["editBoardColumnName"]) => {
+    const column = appState.columns[id];
+    if (!column) return null;
+
+    const newState = produce(appState, (draft) => {
+      draft.columns[id].name = name;
+    });
+    setState(newState);
+    return newState;
+  };
+
+  const handleNewTask = ({
+    columnId,
+    taskName,
+    description,
+  }: ActionPayload["newTask"]) => {
+    const id = getUniqId("task-");
+    const createdAt = DateTime.now().toISO();
+    const task: Task = {
+      title: taskName,
+      id,
+      columnId,
+      description,
+      createdAt,
+      history: [createdAt],
+    };
+    const newState = produce(appState, (draft) => {
+      draft.tasks[id] = task;
+      draft.columns[columnId]?.tasksIds.push(id);
     });
     setState(newState);
     return newState;
@@ -55,6 +95,14 @@ export const useDispatch = () => {
       case "newBoardColumn": {
         type Payload = ActionPayload["newBoardColumn"];
         return handleNewBoardColumn(payload as Payload);
+      }
+      case "editBoardColumnName": {
+        type Payload = ActionPayload["editBoardColumnName"];
+        return handleEditBoardColumnName(payload as Payload);
+      }
+      case "newTask": {
+        type Payload = ActionPayload["newTask"];
+        return handleNewTask(payload as Payload);
       }
 
       default:
